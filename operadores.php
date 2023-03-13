3e9196f028f272f0a7cffe2272585ce50519337e
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-03-13 14:47:16 -0300

    operadores aritméticos

---
+++ operadores.php
@@ -0,0 +1,33 @@
+<?php
+
+$dez = 10;
+//$vinte = 20;
+$trinta = 30;
+
+//$resultado = $dez * $vinte;
+//echo $resultado; // operadores aritméticos
+
+$valor1 = 20;
+$valor2 = 20;
+
+//$teste = $valor1 == $valor2; //comparação. o == serve pra dizer se os dois valores são iguais ou n;
+//echo $teste // = 1: true;
+
+$valor3 = '20';
+
+//$teste = $valor1 === $valor3; //retorna false, porque compara também o tipo
+//echo $teste; // == compara somente os valores, então retornaria true
+
+//!= se usar pra verificar se o valor é diferente, desconsiderando tipo; ex.:
+
+//$teste = $valor1 != $dez //retorna 1 - true;
+
+//pra que seja considerado o tipo, se usa !==
+//também se usa 'menor que' (<) e 'maior que' (>)
+
+//$teste = $valor1 && $dez; -> serve pra verificar se os dois valores são true
+
+// || -> or; se um OU outro é verdadeiro;
+//se um dos dois for verdadeiro, retorna true;
+
+//! serve pra inverter -> o que é true vira false e vice e versa
