d96669b805a8bad92065149c72a1fed286c10897
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-03-13 14:05:56 -0300

    variaveis e constantes no php

---
+++ variaveis_e_constantes.php
@@ -0,0 +1,22 @@
+<?php
+//comentário é que nem no js
+// declara variavel com $
+$nome = 'João Oliveira';
+$inteiro = 10000;
+$preco = 100.50;
+$lista = [10.20, 50, 40.23];
+$eh_verdade = true;
+$variavel110 = 10;
+
+echo $nome;
+
+var_dump($lista);
+
+//const
+
+define('pi', 3.1415); //define é o const. Ele define o valor da atribuição
+
+echo pi;
+var_dump(pi);
+ //no php, assim como no js, não precisa declarar o tipo das variáveis;
+ 
