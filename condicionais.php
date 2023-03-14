27a2f5cf95fd30e36829254d311761ae9a489b10
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-03-14 09:34:19 -0300

    if else

---
+++ condicionais.php
@@ -0,0 +1,24 @@
+<?php
+
+//$nota_do_aluno = 6;
+
+//if($nota_do_aluno >= 6) {
+	//echo "Aprovado!!";
+//} //se a condição for true, ele retorna o que tá no laço (chaves)
+
+//$senha = '123123';
+//	if($senha == '123456') {
+//	echo 'Senha correta!!';
+//} else {
+//	echo 'Senha Incorreta';
+//} //como usar o else: depois do fechamento da chave.
+
+$numero = 200;
+
+if($numero > 0) {
+	echo 'positivo';
+} else if($numero < 0) {
+	echo 'negativo';
+} else {
+	echo 'número é zero';
+} // else if -> "ou se"
